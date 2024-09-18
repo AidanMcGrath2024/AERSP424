ad65f39c91a069f9e6b90f32afd1ef4f14d2e54c
AidanMcGrath2024 <awm6052@psu.edu>
2024-09-18 12:36:37 -0400

    Update Homework1Question1.C
    This should be a working file for question 1.

--- Homework1Question1.C
+++ Homework1Question1.C
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 int main() {
-  /**/
   //Plane Limitations
   int maxallowablegrossweight = 2950; //pounds
   int forwardCGlimit = 82.1; //inches
@@ -10,74 +9,118 @@ int main() {
   //Airplane Empty Weight User Input
   printf("What is the empty weight of the airplane? Type the weight in pounds and press enter: \n");
   int emptyweight;
-  scanf("%i", &emptyweight);
+  scanf("%d", &emptyweight);
 
   //Airplane Empty Weight Moment User Input
   printf("What is the empty weight moment of the airplane? Type the weight in pounds-inches and press enter: \n");
   int emptyweightmoment;
-  scanf("%i", &emptyweightmoment);
+  scanf("%d", &emptyweightmoment);
   
-  //Front Seat Occupant Total and Weight User Inputs
+  //Front Seat Occupant Total Weight User Input
   printf("How many passengers will be flying in the front? Type the number of passangers and press enter: \n");
   int frontseatoccupants;
-  scanf("%i", &frontseatoccupants);
-  int frontseatoccupantsweightarray[frontseatoccupants];
+  scanf("%d", &frontseatoccupants);
+  int weightfrontseatoccupants = 0;
   for (int frontpassengerindex = 0; frontpassengerindex < (frontseatoccupants+1); frontpassengerindex++)
   {
     int weightoffrontseatoccupant;
-    printf("What is the weight of passenger %i in the front? Type the weight of passanger in pounds and press enter: \n",frontpassengerindex);
-    scanf("%i", &weightoffrontseatoccupant);
-    frontseatoccupantsweightarray[frontpassengerindex] = weightoffrontseatoccupant;
+    printf("What is the weight of passenger %d in the front? Type the weight of passanger in pounds and press enter: \n",frontpassengerindex);
+    scanf("%d", &weightoffrontseatoccupant);
+    weightfrontseatoccupants = weightfrontseatoccupants + weightoffrontseatoccupant;
   }
 
   //Front Seat Moment Arm User Input
   printf("What is the front seat moment arm? Type the length in inches and press enter: \n");
   int frontseatmomentarm;
-  scanf("%i", &frontseatmomentarm);
+  scanf("%d", &frontseatmomentarm);
 
-  //Rear Seat Occupant Total and Weight User Inputs
+  //Rear Seat Occupant Total Weight User Input
   printf("How many passengers will be flying in the rear? Type the number of passangers and press enter: \n");
   int rearseatoccupants;
-  scanf("%i", &rearseatoccupants);
-  int rearseatoccupantsweightarray[rearseatoccupants];
+  scanf("%d", &rearseatoccupants);
+  int weightrearseatoccupants = 0;
   for (int rearpassengerindex = 0; rearpassengerindex < (rearseatoccupants+1); rearpassengerindex++)
   {
     int weightofrearseatoccupant;
-    printf("What is the weight of passenger %i in the rear? Type the weight of passanger in pounds and press enter: \n",rearpassengerindex);
-    scanf("%i", &weightofrearseatoccupant);
-    rearseatoccupantsweightarray[rearpassengerindex] = weightofrearseatoccupant;
+    printf("What is the weight of passenger %d in the rear? Type the weight of passanger in pounds and press enter: \n",rearpassengerindex);
+    scanf("%d", &weightofrearseatoccupant);
+    weightrearseatoccupants = weightrearseatoccupants + weightofrearseatoccupant;
   }
 
   //Rear Seat Moment Arm User Input
   printf("What is the front seat moment arm? Type the length in inches and press enter: \n");
   int rearseatmomentarm;
-  scanf("%i", &rearseatmomentarm);
+  scanf("%d", &rearseatmomentarm);
 
   //Gallons of Fuel User Input
   printf("How many gallons of fuel is usable? Type the volume of fuel in gallons and press enter: \n");
   int usablefuel;
-  scanf("%i", &usablefuel);
+  scanf("%d", &usablefuel);
 
   //Usable Fuel Weight per Gallon User Input
   printf("What is the weight per gallon of the usable fuel? Type the conversion in pounds per gallon and press enter: \n");
   int fuelweightpergallon;
-  scanf("%i", &fuelweightpergallon);
+  scanf("%d", &fuelweightpergallon);
 
   //Fuel Tank Moment Arm User Input
   printf("What is the fuel tank's moment arm? Type the length in inches and press enter: \n");
   int fueltankmomentarm;
-  scanf("%i", &fueltankmomentarm);
+  scanf("%d", &fueltankmomentarm);
 
   //Baggage Weight User Input
   printf("What is the total baggage weight? Type the weight in pounds and press enter: \n");
   int baggageweight;
-  scanf("%i", &baggageweight);
+  scanf("%d", &baggageweight);
 
   //Baggage Moment Arm User Input
   printf("What is the baggage weight's moment arm? Type the length in inches and press enter: \n");
   int baggagemomentarm;
-  scanf("%i", &baggagemomentarm);
+  scanf("%d", &baggagemomentarm);
 
+  int grossweight = emptyweight + weightfrontseatoccupants + weightrearseatoccupants + (usablefuel*fuelweightpergallon) + baggageweight;
+  float centerofgravity = ((emptyweight*emptyweightmoment) + (frontseatmomentarm*weightfrontseatoccupants) + (rearseatmomentarm*weightrearseatoccupants) + (fueltankmomentarm*usablefuel*fuelweightpergallon) + (baggagemomentarm*baggageweight))/(grossweight);
+  if (centerofgravity < forwardCGlimit)
+  {
+    double newusablefuel = (((emptyweight*emptyweightmoment) + (frontseatmomentarm*weightfrontseatoccupants) + (rearseatmomentarm*weightrearseatoccupants) + (baggagemomentarm*baggageweight)) - (aftCGlimit * (emptyweight + weightfrontseatoccupants + weightrearseatoccupants + baggageweight)))/(aftCGlimit*fuelweightpergallon - fueltankmomentarm*fuelweightpergallon);
+    grossweight = emptyweight + weightfrontseatoccupants + weightrearseatoccupants + (newusablefuel*fuelweightpergallon) + baggageweight;
+    double centerofgravity = ((emptyweight*emptyweightmoment) + (frontseatmomentarm*weightfrontseatoccupants) + (rearseatmomentarm*weightrearseatoccupants) + (fueltankmomentarm*newusablefuel*fuelweightpergallon) + (baggagemomentarm*baggageweight))/(grossweight);
+    double drainedfuel = (newusablefuel-usablefuel)*fuelweightpergallon;
+    double precdrainedfuel = round(drainedfuel*100)/100;
+    if (grossweight > maxallowablegrossweight)
+    {
+      printf("Your loading is not within the aircraft's tolerances!\n");
+    }
+    else
+    {
+      printf("The amount of fuel drained is %f gallons. \n", (precdrainedfuel));
+      printf("The new gross weight of the aircraft is: %f pounds.\n",grossweight);
+      printf("The new center of gravity of the aircraft is: %f inches from the engine's firewall.\n",centerofgravity);
+    }
+  }
+  else if (centerofgravity > aftCGlimit)
+  {
+    double newusablefuel = (((emptyweight*emptyweightmoment) + (frontseatmomentarm*weightfrontseatoccupants) + (rearseatmomentarm*weightrearseatoccupants) + (baggagemomentarm*baggageweight)) - (aftCGlimit * (emptyweight + weightfrontseatoccupants + weightrearseatoccupants + baggageweight)))/(aftCGlimit*fuelweightpergallon - fueltankmomentarm*fuelweightpergallon);
+    grossweight = emptyweight + weightfrontseatoccupants + weightrearseatoccupants + (newusablefuel*fuelweightpergallon) + baggageweight;
+    double centerofgravity = ((emptyweight*emptyweightmoment) + (frontseatmomentarm*weightfrontseatoccupants) + (rearseatmomentarm*weightrearseatoccupants) + (fueltankmomentarm*newusablefuel*fuelweightpergallon) + (baggagemomentarm*baggageweight))/(grossweight);
+    double addedfuel = (newusablefuel-usablefuel)*fuelweightpergallon;
+    double precaddedfuel = round(addedfuel*100)/100;
+    if (grossweight > maxallowablegrossweight)
+    {
+      printf("Your loading is not within the aircraft's tolerances!\n");
+    }
+    else
+    {
+      printf("The amount of fuel added is %f pounds. \n", (precaddedfuel));
+      printf("The new gross weight of the aircraft is: %f pounds.\n",grossweight);
+      printf("The new center of gravity of the aircraft is: %f inches from the engine's firewall.\n",centerofgravity);
+    }
+  }
+  else 
+  {
+    printf("The gross weight of the aircraft is %f pounds.\n", grossweight);
+    printf("Fuel amount was not adjusted, the center of gravity is within the aircraft's limits: %f inches from the engine's firewall.\n", centerofgravity);
+  }
+  /*
   int emptyweight = 2050; //pounds
   int emptyweightmoment = 155400; //pound*inches
   int frontseatoccupants = 2; 
@@ -95,12 +138,6 @@ int main() {
   int maxallowablegrossweight = 2950; //pounds
   int forwardCGlimit = 82.1; //inches
   int aftCGlimit = 84.7; //inches 
-  if (grossweight < maxallowablegrossweight)
-  {
-    usablefuel = maxallowablegrossweight - grossweight; //pounds
-    printf("The new gross weight of the aircraft is: %f pounds",grossweight);
-
-
-  }
+  */
   return 0;
 }
